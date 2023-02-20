edc3f0ac0948dab2fdba47c6de54ae75e3f42af4
topurayhan007 <topurayhan007@gmail.com>
2023-02-20 17:50:41 +0600

    practice problem done

--- practice-problems/practice-problem-4.js
+++ practice-problems/practice-problem-4.js
@@ -1,7 +1,7 @@
 // arrow func, take 2 array, combine, find Max from combined array
 const combineAndFindMax = (arr1, arr2) => {
   const newArr = [...arr1, ...arr2];
-  console.log(newArr);
+
   const max = Math.max(...newArr);
   return max;
 };
