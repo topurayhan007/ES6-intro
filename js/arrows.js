415bdb392ecf66f97f4f0c94db5f9c289028c362
topurayhan007 <topurayhan007@gmail.com>
2023-02-20 16:25:46 +0600

    incremental update

--- js/arrows.js
+++ js/arrows.js
@@ -18,11 +18,19 @@ const add2 = function (first, second) {
   return total;
 };
 
-function add(first, second) {
-  const total = first + second;
-  return total;
+function add4(first, second) {
+  return first + second;
 }
+
+const add5 = function (first, second) {
+  return first + second;
+};
+
+// arrow function
+const add6 = (first, second) => first + second;
 
 const result = add(10, 20);
 console.log(result);
 
+// interview question: differences between
+// function declaration, function expression and arrow function
